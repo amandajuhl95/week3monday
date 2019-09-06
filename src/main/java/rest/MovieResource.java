894f87d413824664ed5e356ec2f33eac9f2841a2
Amanda <Manda_Hansen@hotmail.com>
2019-09-06 14:07:00 +0200

    change drop_and_create to create

--- src/main/java/rest/MovieResource.java
+++ src/main/java/rest/MovieResource.java
@@ -19,7 +19,7 @@ import javax.ws.rs.core.MediaType;
 @Path("movie")
 public class MovieResource {
 
-    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.DEV, EMF_Creator.Strategy.DROP_AND_CREATE);
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.DEV, EMF_Creator.Strategy.CREATE);
     private static final MovieFacade FACADE = MovieFacade.getFacadeExample(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
